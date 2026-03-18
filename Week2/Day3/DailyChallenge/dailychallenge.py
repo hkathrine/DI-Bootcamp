ee742f8fe7ded9209beee0dfcfb50a7963bf023e
hkathrine <h.kathrine.v@gmail.com>
2026-03-18 17:13:54 +0300

    commit4

--- Week2/Day3/DailyChallenge/dailychallenge.py
+++ Week2/Day3/DailyChallenge/dailychallenge.py
@@ -1,9 +1,19 @@
 import random
-list_if_numbers = [random.randint(0, 10000) for _ in range(20000)]
+
+
+list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 target_number = 3728
 count = 0
-for i in range(20000):
-    for j in range(20000 - i):
-        if i != j and ((list_if_numbers[i] + list_if_numbers[i + j]) == target_number):
-            count = count + 1
-            print(f"{list_if_numbers[i]} and {list_if_numbers[j]} sums to the target number {target_number}")
+
+seen_numbers = {}
+
+for num in list_of_numbers:
+    complement = target_number - num
+    
+    if complement in seen_numbers:
+        count += seen_numbers[complement]
+    
+    # Запоминаем текущее число
+    seen_numbers[num] = seen_numbers.get(num, 0) + 1
+
+print(f"Total pairs found: {count}")
