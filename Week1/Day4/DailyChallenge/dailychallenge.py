32a21c6ce151c9cfc3b59c92cb239cc38e19c6d3
hkathrine <h.kathrine.v@gmail.com>
2026-03-19 01:49:22 +0300

    commit 6

--- Week1/Day4/DailyChallenge/dailychallenge.py
+++ Week1/Day4/DailyChallenge/dailychallenge.py
@@ -1,1 +1,35 @@
+#1
+def get_input(parameter):
+    while True:
+        num = input(f"Please enter a {parameter}: ")
+        try:
+            val = int(num)
+        except ValueError:
+            print("The input is invalid")
+            continue
+        return val
+    
+
+number = get_input("number")
+length = get_input("length")
+
+num_list = [number * x for x in range(1,length + 1)]
+print(num_list)
+
+#2
+str = input("please enter a string: ")
+
+str_modified = []
+
+current_letter = str[0]
+str_modified.append(str[0])
+for letter in str:
+    if letter == current_letter:
+        continue
+    else:
+        str_modified.append(letter)
+        current_letter = letter
+
+print("".join(str_modified))
+
 
