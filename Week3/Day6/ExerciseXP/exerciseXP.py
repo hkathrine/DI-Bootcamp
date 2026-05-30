6171f847971a678daa61d401d095dd6f4d02e964
hkathrine <h.kathrine.v@gmail.com>
2026-05-31 01:08:53 +0300

    one file

--- Week3/Day6/ExerciseXP/exerciseXP.py
+++ Week3/Day6/ExerciseXP/exerciseXP.py
@@ -1,4 +1,20 @@
 import random
+import json
+
+json_string = """
+{
+    "company": {
+        "name": "TechCorp",
+        "employee": {
+            "name": "Alex",
+            "payable": {
+                "salary": 5000,
+                "currency": "USD"
+            }
+        }
+    }
+}
+"""
 
 #ex 1
 def get_words_from_file(file_name):  
@@ -19,13 +35,27 @@ def get_random_sentence(sentence_length):
     return sentence.lower()
 
 def main():
+    #ex 1
     print("Th program generates a sentence from random words.")
     sentence_length = input("Please enter the desirable sentence length:")
     if sentence_length.isdigit() and int(sentence_length) >= 2 and int(sentence_length) <= 20:
         print(get_random_sentence(int(sentence_length)))
     else:
         print("The input is invalid")
-    
+    #ex 2
+    data = json.loads(json_string)
+    current_salary = data["company"]["employee"]["payable"]["salary"]
+    print(f"Current salary: {current_salary}")
+
+    data["company"]["employee"]["birth_date"] = "1812-05-15"
+
+    with open("modified_data.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
+
+    print("The file is saves as 'modified_data.json'")
+
 
 
 main()
+
+
